b68500fd8e0a03a479a16fba5471b38dac638aa4
Adrian Mowat <adrian.mowat@gmail.com>
2018-11-23 09:11:38 +0000

    Add drag and copy icons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 function dragstart_handler(ev) {
-  ev.dataTransfer.setData("text/plain", ev.target.textContent);
+  ev.dataTransfer.setData("text/plain", ev.target.dataset.text);
+}
+
+function copyText(ev) {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
 }
